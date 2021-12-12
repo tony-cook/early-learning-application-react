4562758c0473f810aeef00ce158ea7937be7a2c3
Tony Cook <tony_cook@outlook.com>
2021-12-12 14:15:42 +1300

    created Footer component

--- main/src/pages/Projects/Projects.js
+++ main/src/pages/Projects/Projects.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../../shared/NavBar/NavBar'
+import Footer from '../../shared/Footer/Footer'
 
 function Projects() {
     return (
         <div>
             <NavBar />
+            <Footer />
         </div>
     )
 }
---
+++ main/src/shared/Footer/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Footer.css';
+
+function Footer() {
+    return (
+        <>
+            <div class= "footer-container">
+                <div class="flexbox">
+                    <div class="text-group" >
+                        <span id="title">COMPANY</span> <br/><br/> About Us <br/>Careers <br/> Partners
+                    </div>
+                    <div class="text-group" >
+                        <span id="title">COURSES</span> <br/><br/> Register <br/> Login <br/> Projects <br/>
+                        Teachers <br/> Parents <br/> Resources
+                    </div>
+                    <div class="text-group" >
+                        <span id="title">SUPPORT</span> <br/><br/> FAQs <br/> Helpdesk <br/> Contact Us <br/>
+                    </div>
+                    <div class="text-group" >
+                        <span id="title">LEGAL</span> <br/><br/> Terms & Conditions <br/> Privacy Policy
+                    </div>
+                    <div class="text-group lg" >
+                        <span id="title">LevelUp Works</span> <br/>LevelUp Works is an Auckland-based enterprise dedicated 
+                        to developing game-based learning software to help teachers in response to the New Zealand Digital Technologies 
+                        & Hangarau Matihiko.<br/> alan@levelupworks.com <br/>(021) 668 185
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
