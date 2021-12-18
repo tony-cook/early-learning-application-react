c562faf5c822bffb1b5642061339fb6cc7c69315
Ray Letoa <ray.letoa@gmail.com>
2021-12-18 14:39:39 +1300

    will be changing file structure

--- main/src/Pages/TestPages.js
+++ main/src/Pages/TestPages.js
@@ -4,6 +4,7 @@ import TEST from '../Components/test'
 export default function TestPages() {
     return (
         <div>
+            will be changing file structure for git pushes
             <TEST />
         </div>
     )
