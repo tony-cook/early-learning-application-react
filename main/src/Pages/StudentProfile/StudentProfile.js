60704cfd9fafb273915c48b711120cbddc054be6
Ray Letoa <rayletoa@RAYway-2.hub>
2021-12-21 19:29:18 +1300

    have added Tonys script for local storage

--- main/src/Pages/StudentProfile/StudentProfile.js
+++ main/src/Pages/StudentProfile/StudentProfile.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import StudentProfileHeader from '././Components/StudentProfileHeader'
 import StudentProfileBody from '././Components/StudentProfileBody'
 import StudentProfileFooter from '././Components/StudentProfileFooter'
@@ -6,12 +6,26 @@ import StudentProfileFooter from '././Components/StudentProfileFooter'
 
 export default function StudentProfile() {
 
-    const id=1
+    useEffect(() => {
+        localStorage.clear();
+            let teacherId = prompt("Enter the teacher_id", "1")
+            if (teacherId === null || teacherId === "") {
+                let studentID = prompt("Enter the student_id", "1")
+                alert(`You have logged in as student_id ${studentID}`)
+                localStorage.setItem('studentID', studentID)
+            } else {
+                alert(`You have logged in as teacher_id ${teacherId}`)
+                localStorage.setItem('userIdTeacher', teacherId)
+            }
+    },[])
+
+    const studentID = localStorage.getItem('studentID')
+    const teacherId = localStorage.getItem('teacherID')
 
     return (
         <div className='holder-box'>
-            <StudentProfileHeader id={id}/>
-            <StudentProfileBody id={id}/>
+            <StudentProfileHeader />
+            <StudentProfileBody />
             <StudentProfileFooter />
         </div>
     )
