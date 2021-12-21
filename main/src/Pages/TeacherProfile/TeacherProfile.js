8749d9c7c5c6ca4f2f179c23800ff43400122e15
Ray Letoa <rayletoa@RAYway-2.hub>
2021-12-21 19:45:35 +1300

    Have added localstorage script

--- main/src/Pages/TeacherProfile/TeacherProfile.js
+++ main/src/Pages/TeacherProfile/TeacherProfile.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import TeacherProfileHeader from '././Components/TeacherProfileHeader'
 import TeacherProfileFooter from '././Components/TeacherProfileFooter'
 import TeacherProfileBody from '././Components/TeacherProfileBody'
 // import '../Components/CSS/body.css'
 
-export default function StudentProfile() {
+export default function TeacherProfile() {
 
-    const id = 5
+    useEffect(() => {
+        localStorage.clear();
+            let teacherId = prompt("Enter the teacher_id", "1")
+            if (teacherId === null || teacherId === "") {
+                let studentID = prompt("Enter the student_id", "1")
+                alert(`You have logged in as student_id ${studentID}`)
+                localStorage.setItem('studentID', studentID)
+            } else {
+                alert(`You have logged in as teacher_id ${teacherId}`)
+                localStorage.setItem('teacherID', teacherId)
+            }
+    },[])
+
+    const studentID = localStorage.getItem('studentID')
+    const teacherId = localStorage.getItem('teacherID')
 
     return (
         <div className='holder-box'>
-            <TeacherProfileHeader id={id}/>
-            <TeacherProfileBody id={id}/>
+            <TeacherProfileHeader />
+            <TeacherProfileBody />
             <TeacherProfileFooter />
         </div>
     )
