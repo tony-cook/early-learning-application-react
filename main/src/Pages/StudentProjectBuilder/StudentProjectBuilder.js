ab1ed24916707675180720e884776e41dc13487d
Ray Letoa <rayletoa@RAYway-2.hub>
2021-12-21 19:38:28 +1300

    adding tonys script to put in localstorage our student id

--- main/src/Pages/StudentProjectBuilder/StudentProjectBuilder.js
+++ main/src/Pages/StudentProjectBuilder/StudentProjectBuilder.js
@@ -1,22 +1,32 @@
-import React from 'react'
-// import '../Components/CSS/body.css'
+import React, {useEffect} from 'react'
 import StudentProjectHeader from '././Components/StudentProjectHeader'
 import StudentProjectBody from '././Components/StudentProjectBody'
 import StudentProjectFooter from '././Components/StudentProjectFooter'
 
 export default function StudentProjectBuilder() {
 
-    const id=9 //ID 9 is the best
+    useEffect(() => {
+        localStorage.clear();
+            let teacherId = prompt("Enter the teacher_id", "1")
+            if (teacherId === null || teacherId === "") {
+                let studentID = prompt("Enter the student_id", "1")
+                alert(`You have logged in as student_id ${studentID}`)
+                localStorage.setItem('studentID', studentID)
+            } else {
+                alert(`You have logged in as teacher_id ${teacherId}`)
+                localStorage.setItem('teacherID', teacherId)
+            }
+    },[])
+
+    const studentID = localStorage.getItem('studentID')
+    const teacherId = localStorage.getItem('teacherID')
 
     return (
 
         <div className='holder-box'>
-            <StudentProjectHeader id={id}/>
-            <StudentProjectBody id={id}/>
+            <StudentProjectHeader />
+            <StudentProjectBody />
             <StudentProjectFooter />
-            
-            
-        
         </div>
         
     )
