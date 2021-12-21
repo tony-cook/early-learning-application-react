ed5ed5f29fcb440a0ea85b2c529a19ba9805c545
Tony Cook <tony_cook@outlook.com>
2021-12-21 16:00:41 +1300

    added new images to public folder

--- main/src/App/App.js
+++ main/src/App/App.js
@@ -1,16 +1,18 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './App.css';
-import StudentProjects from '../pages/StudentProjects/StudentProjects'
+import StudentProjects from '../pages/studentProjects/StudentProjects'
 import TeacherProjects from '../pages/Projects/TeacherProjects'
+import StudentProfile from '../pages/StudentProfile/StudentProfile';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<StudentProfile />} />
           <Route path="/student/projects" element={<StudentProjects />} />
-        <Route path="/teacher/projects" element={<TeacherProjects />} />
+          <Route path="/teacher/projects" element={<TeacherProjects />} />
       </Routes>
       </BrowserRouter>
     </>
