980a79c9fcdd613d15162052a29333da520a7100
Ray Letoa <rayletoa@RAYway-2.hub>
2021-12-21 22:03:46 +1300

    correct links for and text for the footer

--- main/src/Pages/StudentProfile/Components/StudentProfileFooter.js
+++ main/src/Pages/StudentProfile/Components/StudentProfileFooter.js
@@ -21,24 +21,19 @@ export default function StudentProfileHeader() {
                     <span className='spacer'>Resources</span>
                 </span>
                 <span className='footerColumn'>
-                    <span className='spacerTitle'>COURSES</span>
-                    <span className='spacer'>Register</span>
-                    <span className='spacer'>Login</span>
-                    <span className='spacer'>Teachers</span>
-                    <span className='spacer'>Parents</span>
-                    <span className='spacer'>Resources</span>
+                    <span className='spacerTitle'>SUPPORT</span>
+                    <span className='spacer'>FAQs</span>
+                    <span className='spacer'>Helpdesk</span>
+                    <span className='spacer'>Contact Us</span>
                 </span>
                 <span className='footerColumn'>
-                    <span className='spacerTitle'>COURSES</span>
-                    <span className='spacer'>Register</span>
-                    <span className='spacer'>Login</span>
-                    <span className='spacer'>Teachers</span>
-                    <span className='spacer'>Parents</span>
-                    <span className='spacer'>Resources</span>
+                    <span className='spacerTitle'>LEGAL</span>
+                    <span className='spacer'>Terms &amp;     Conditions</span>
+                    <span className='spacer'>Privacy Policy</span>
                 </span>
                 <span className='footerColumn'>
-                    <span className='spacerTitle'>COURSES</span>
-                    <span className='spacer'>LevelUp Works LevelUp Works is an Auckland-based enterprise dedicated to developing game-based learning software to help teachers in response to the New Zealand Digital Technologies & Hangarau Matihiko. alan@levelupworks.com (021) 668 185</span>
+                    <span className='spacerTitleLast'>LevelUp Works</span>
+                    <span className='spacerLast'>LevelUp Works LevelUp Works is an Auckland-based enterprise dedicated to developing game-based learning software to help teachers in response to the New Zealand Digital Technologies & Hangarau Matihiko. alan@levelupworks.com (021) 668 185</span>
                 </span>
             </div>
         </div>
